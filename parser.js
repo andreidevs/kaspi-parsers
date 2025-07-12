bdcdbaf800ccd53e1708ef6ef0c030df4d484e9c
Andrei Eronin <andrei.dev2016@yandex.ru>
2025-07-12 21:29:01 +0500

    f

--- parser.js
+++ parser.js
@@ -343,7 +343,7 @@ function logProcessedId(id) {
             '--disable-features=VizDisplayCompositor',
             '--single-process'
         ],
-        executablePath: process.platform === 'linux' ? '/usr/bin/chromium-browser' : undefined
+        // executablePath: process.platform === 'linux' ? '/usr/bin/chromium-browser' : undefined
     });
 
     let completedRequests = 0;
